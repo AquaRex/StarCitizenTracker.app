214fd3dd80be3dfd9c86b8e5d2feb2d3aeb50fed
Thomas <Tomyf4@gmail.com>
2025-10-30 19:36:57 +0100

    Added download button, and version number to download page

--- components/navigation.js
+++ components/navigation.js
@@ -39,6 +39,14 @@ function generateNavigation(options = {}) {
                     </button>
                 ` : ''}
                 
+                <button class="btn btn-icon" onclick="window.location.href='download/'" title="Download">
+                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                </button>
+                
                 <button class="btn btn-icon" id="darkModeToggle" title="Toggle Dark Mode">
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <circle cx="12" cy="12" r="5"></circle>
